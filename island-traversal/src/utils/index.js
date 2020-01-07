388c067a9a1de5e8bc167f6a5c3fe0653d593fcb
Dustin <clerin+gitkraken@gmail.com>
2020-01-07 10:03:22 -0800

    WIP local storage for previous room and travel dir

--- island-traversal/src/utils/index.js
+++ island-traversal/src/utils/index.js
@@ -4,6 +4,16 @@ import * as info from './info';
 function setCurrentRoom(rm_id) {
     localStorage.setItem( "curr_rm_id", rm_id )
 }
+function setPrevRoom(prev_rm_id) {
+    localStorage.setItem( "prev_rm", prev_rm_id )
+}
+function setTravelDir(dir) {
+    localStorage.setItem( "travel_dir", dir )
+}
+function getPrevRoom() {
+    let prevRm = null;
+    if(localStorage.getItem("prev_"))
+}
 
 function checkIfRoomStored() {
     let curRoomId = null;
