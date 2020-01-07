c9d823100d8de7dfae72bd2a153b5145ac70e432
kingmoc <joe.c.a.garcia@gmail.com>
2020-01-07 00:38:36 -0600

    Init React Setup

---
+++ island-traversal/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  function traverseMap() {
+    // if you don't know where you are
+        // init to get room id
+    // create room
+    // pick a direction not traveled
+        // travel direction
+    // if all directions traveled
+        // get all rooms
+        // find room with unexplored directions
+        // find quickest route there
+            // store route and travel to room
+    // add room
+  }
+
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Welcome </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
